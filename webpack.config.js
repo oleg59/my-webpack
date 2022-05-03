07cc2c14fa45442db7a958d955811c511be7eb86
oleg59 <web-perm@bk.ru>
2022-05-04 04:16:51 +0500

    add favicon and img

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -31,5 +33,11 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.css',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "src/img", to: "img" },
+            ],
+        }),
+        new CleanWebpackPlugin()
     ],
 }
